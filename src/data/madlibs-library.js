2e2bde344516129cf3dc53205543dcc6ba4f5e54
worldspawn <worldspawn.web@gmail.com>
2023-06-21 21:00:33 +0300

    more stories for madlibs added

--- src/data/madlibs-library.js
+++ src/data/madlibs-library.js
@@ -3,7 +3,9 @@ import clc from 'cli-color';
 const getStorylines = () => {
     const storylines = [
         'Zoo Day',
-        'Amusement Park',
+        'The Fun Park',
+        'At the Arcade',
+        'The First Day of School',
     ];
     let output = `${clc.yellow('Available storylines:')}\n`;
     for (let i = 0; i < storylines.length; i += 1) {
@@ -12,11 +14,9 @@ const getStorylines = () => {
     return output;
 }
 
-const getStory = (number) => {
-    // zoo madlib
-    const story1 = [
-        '\nToday I went to the zoo. I saw a(n) (...)',
-        '(...) jumping up and down in its tree.',
+const stories = {
+    1: [
+        '\nToday I went to the zoo. I saw a(n) (...) jumping up and down in its tree.',
         '\nHe (...) through the large tunnel',
         'that led to its (...).',
         '\nI got some peanuts and passed them through the cage to a gigantic gray (...) towering above my head.',
@@ -25,9 +25,8 @@ const getStory = (number) => {
         'and (...) to catch our bus.',
         '\nWhen I got home I (...) my mom',
         'for a (...) day at the zoo.',
-    ];
-    // park madlib
-    const story2 = [
+    ],
+    2: [
         '\nToday, my fabulous camp group went to a(an) (...) amusement park.',
         '\nIt was a fun park with lots of cool (...) and enjoyable play structures.',
         '\nWhen we got there, my kind counselor shouted loudly, "Everybody off the (...)!"',
@@ -38,15 +37,43 @@ const getStory = (number) => {
         '\nIn about two minutes I heard the crank and grinding of the gears. That is when the ride began! When I got to the bottom, I was a little (...) but I was proud of myself.',
         '\nThe rest of the day went (...).',
         '\nIt was a(n) (...) day at the fun park.',
+    ],
+    3: [
+        '\nWhen I go to the arcade with my (...) there are lots of games to play.',
+        '\nI spend lots of time there with my friends. In the game X-Men you can be different (...).',
+        '\nThe point of the game is to (...) every robot.',
+        '\nYou also need to save people. Then you can go to the next level.\n\nIn the game Star Wars you are Luke Skywalker and you try to destroy every (...).',
+        '\nIn a car racing/motorcycle racing game you need to beat every computerized vehicle that you are (...) against.',
+        '\nThere are a whole lot of other cool games. When you play some games you win (...) for certain scores.',
+        '\nOnce you are done - you can cash in your tickets to get a big (...)',
+        '\nYou can save your (...) for another time.',
+        '\nWhen I went to this arcade I did not believe how much (...) it would be. So far I have had a lot of fun every time I have been to this great arcade!',
+    ],
+    4: [
+        '\nOne very nice morning near the end of summer, my mother woke me up at 4:00 A.M. and said:\n\n- Wake up and smell the grass, sleepy head! Today is your first day of school and you can not be late!\n\nI groaned in my bed for twenty seconds, but eventually I got dressed.\nI wore a blue and white striped, long sleeve (...) with a collar on it, a red tie, dark grey pants, white socks, black shoes.',
+        '\nAlso I had a (...) hat.',
+        '\nIn ten minutes I made a lunch and ate my breakfast. (...) minutes later, the bus came.',
+        '\n(...) later, I was at school.',
+        '\n\nIn school, I met two really (...) kids.',
+        '\nAll of us became friends very fast. That day we had Science, and luckily my friends and I were at the same (...).',
+        '\nFirst guy name was (...).',
+        '\nSecond - (...).',
+        '\nIn Math we were not together, and that really bugged me. We learned what (...) were, and when to use them.',
+        '\nAt snack and recess, we played (...) together. It was extremely fun.',
+        '\nAt P.E., we were (...) off of the ropes',
+        '\nonto (...).',
+        '\nI thought it was a very (...) idea.',
+        '\nIn swimming class, we needed to swim extremely (...), or else we would have to swim longer.',
+        '\nBefore I knew it, school was over. I grabbed all my belongings and put them into my backpack. In two minutes, the bus came. As I stepped into the bus I shouted:\n"(...)" to my friends. Then I went into the bus. In a flash, I was back home. This day was an extremely exciting day!',
     ]
+};
 
-    switch (number) {
-        case '1':
-            return story1;
-        case '2':
-            return story2;
-        default:
-            throw new Error(`Story value is invalid!`)
+
+const getStory = (number) => {
+    if (stories[number]) {
+        return stories[number];
+    } else {
+        throw new Error(`${number} value is invalid!`);
     }
 }
 

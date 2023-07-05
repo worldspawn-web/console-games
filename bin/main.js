18c638c9808a9115c720ae5d2bf80b83a1f3801a
Canfus <canfus69@gmail.com>
2023-07-06 01:42:12 +0500

    find your bug

--- bin/main.js
+++ bin/main.js
@@ -1,34 +1,48 @@
-import boxen from 'boxen';
-import readlineSync from 'readline-sync';
+import boxen from "boxen";
+import readlineSync from "readline-sync";
 
-import { diceGameRules } from '../src/diceroll.js';
-import { guessNumberRules } from '../src/guessNumber.js';
-import madLibsRules from '../src/madLibs.js';
-import ticTacToeRules from '../src/tictactoe.js';
-import hangmanRules from '../src/hangman.js';
+import { diceGameRules } from "../src/diceroll.js";
+import { guessNumberRules } from "../src/guessNumber.js";
+import madLibsRules from "../src/madLibs.js";
+import ticTacToeRules from "../src/tictactoe.js";
+import hangmanRules from "../src/hangman.js";
 
 const mainMenu = (username) => {
-    console.clear();
-    console.log(boxen(`\nAvailable games:\n1. Dice Rolling Simulator\n2. Guess the Number\n3. Mad Libs Generator\n4. Tic-Tac-Toe(wip)\n5. Hangman (wip)`, { title: 'Welcome to Simple Console Games!', titleAlignment: 'center', textAlignment: 'center' }));
-    const userChoice = readlineSync.question(`\nWhat is your choice, ${username}?\n`);
-    switch (userChoice) {
-        case '1':
-            diceGameRules(username);
-            break;
-        case '2':
-            guessNumberRules(username);
-            break;
-        case '3':
-            madLibsRules(username);
-            break;
-        case '4':
-            ticTacToeRules(username);
-            break;
-        case '5':
-            hangmanRules(username);
-            break;
-        default:
-            throw new Error(`${userChoice} is an incorrect value!\nPossible values: 0-5`);
-    }
+  console.clear();
+  console.log(
+    boxen(
+      `\nAvailable games:\n1. Dice Rolling Simulator\n2. Guess the Number\n3. Mad Libs Generator\n4. Tic-Tac-Toe(wip)\n5. Hangman (wip)`,
+      {
+        title: "Welcome to Simple Console Games!",
+        titleAlignment: "center",
+        textAlignment: "center",
+      }
+    )
+  );
+  const userChoice = readlineSync.question(
+    `\nWhat is your choice, ${username}?\n`
+  );
+  switch (userChoice) {
+    case "1":
+      diceGameRules(username);
+      break;
+    case "2":
+      guessNumberRules(username);
+      break;
+    case "3":
+      madLibsRules(username);
+      break;
+    case "4":
+      ticTacToeRules(username);
+      break;
+    case "5":
+      hangmanRules(username);
+      break;
+    default:
+      // you need to hook the error with try/catch block or refactor this error
+      throw new Error(
+        `${userChoice} is an incorrect value!\nPossible values: 0-5`
+      );
+  }
 };
 export default mainMenu;
--- src/hangman.js
+++ src/hangman.js
@@ -1,78 +1,100 @@
-import boxen from 'boxen';
-import readlineSync from 'readline-sync';
+import boxen from "boxen";
+import readlineSync from "readline-sync";
 
-import mainMenu from '../bin/main.js';
-import delay from './utils/delay.js';
-import { gameStarts, gameEnds, readyChecker } from './utils/readyChecker.js';
-import { getThemesList, isThemeValid, getThemeName, getThemeWord, hideWord, isValidLetter, correctLetter, containCheck, gameOver } from './data/hangman-stages.js';
+import mainMenu from "../bin/main.js";
+import delay from "./utils/delay.js";
+import { gameStarts, gameEnds, readyChecker } from "./utils/readyChecker.js";
+import {
+  getThemesList,
+  isThemeValid,
+  getThemeName,
+  getThemeWord,
+  hideWord,
+  isValidLetter,
+  correctLetter,
+  containCheck,
+  gameOver,
+} from "./data/hangman-stages.js";
 
 const hangmanRules = async (username) => {
-    console.clear();
-    console.log(boxen(`Hangman is a classic game, where you need to guess the specific word by one letter.\nFor each mistake - your character will be one step closer to being hanged.\nBefore the game starts, you need to choose a word theme (sports, science, movies, etc).\n\nGood luck, ${username}. Save this poor guy.`, { title: 'Hangman', textAlignment: 'center', titleAlignment: 'center' }));
+  console.clear();
+  console.log(
+    boxen(
+      `Hangman is a classic game, where you need to guess the specific word by one letter.\nFor each mistake - your character will be one step closer to being hanged.\nBefore the game starts, you need to choose a word theme (sports, science, movies, etc).\n\nGood luck, ${username}. Save this poor guy.`,
+      { title: "Hangman", textAlignment: "center", titleAlignment: "center" }
+    )
+  );
 
-    await delay(2000);
-    if (readyChecker()) {
-        gameStarts();
-        preHangman(username);
-    } else {
-        gameEnds();
-    }
+  await delay(2000);
+  if (readyChecker()) {
+    gameStarts();
+    preHangman(username);
+  } else {
+    gameEnds();
+  }
 };
 
 const preHangman = async (username) => {
-    console.clear();
-    const themes = getThemesList();
-    console.log(themes);
-    const userThemeNum = readlineSync.question('Choose your theme: ');
-    isThemeValid(userThemeNum) ? startHangman(username, userThemeNum) : console.log(`Theme with number ${userThemeNum} is not valid.\nReturning to main menu...`);
+  console.clear();
+  const themes = getThemesList();
+  console.log(themes);
+  const userThemeNum = readlineSync.question("Choose your theme: ");
+  isThemeValid(userThemeNum)
+    ? startHangman(username, userThemeNum)
+    : console.log(
+        `Theme with number ${userThemeNum} is not valid.\nReturning to main menu...`
+      );
 
-    await delay(3000);
-    mainMenu(username);
+  await delay(3000);
+  mainMenu(username);
 };
 
 const startHangman = async (username, userThemeNum) => {
-    console.clear();
-    const themeName = getThemeName(userThemeNum);
-    console.log(`Selected theme is - ${themeName}.\nPicking up an appropriate word for you...`);
-    const goalWord = getThemeWord(themeName);
-    let goalWordHidden = hideWord(goalWord);
-    let depressionLevel = 0;
-    let correctAnswers = 0;
+  debugger;
+  console.clear();
+  const themeName = getThemeName(userThemeNum);
+  console.log(
+    `Selected theme is - ${themeName}.\nPicking up an appropriate word for you...`
+  );
+  const goalWord = getThemeWord(themeName);
+  let goalWordHidden = hideWord(goalWord);
+  let depressionLevel = 0;
+  let correctAnswers = 0;
 
-    await delay(500);
-    while (depressionLevel < 5 && correctAnswers < goalWord.length) {
-        console.log(`Current depression level: ${depressionLevel}`);
+  await delay(500);
+  while (depressionLevel < 5 && correctAnswers < goalWord.length) {
+    console.log(`Current depression level: ${depressionLevel}`);
 
-        await delay(1000);
-        console.log(`Theme: ${themeName}\n\n${goalWordHidden}`);
+    // await delay(1000);
+    console.log(`Theme: ${themeName}\n\n${goalWordHidden}`);
 
-        await delay(1000);
-        const letterSuggest = readlineSync.keyIn('Letter: ');
-        //
-        // FIX: function returns to the main menu after letter input for some reason
-        //
-        await delay(3000);
-        if (isValidLetter(letterSuggest)) {
-            if (containCheck(letterSuggest, goalWord)) {
-                console.log(`There is no such a letter!`);
-                goalWordHidden = correctLetter(letterSuggest, goalWord, goalWordHidden);
-                correctAnswers += 1;
-            } else {
-                console.log(`Unfortunately, there is no such a letter!`);
-                depressionLevel += 1;
-            }
-        } else {
-            console.log('You have entere not a letter.');
-        }
-
-        await delay(1000);
-    }
-
-    if (depressionLevel === 5) {
-        gameOver('fail', username, goalWord);
+    // await delay(1000);
+    const letterSuggest = readlineSync.keyIn("Letter: ");
+    //
+    // FIX: function returns to the main menu after letter input for some reason
+    //
+    // await delay(3000);
+    if (isValidLetter(letterSuggest)) {
+      if (containCheck(letterSuggest, goalWord)) {
+        console.log(`There is no such a letter!`);
+        goalWordHidden = correctLetter(letterSuggest, goalWord, goalWordHidden);
+        correctAnswers += 1;
+      } else {
+        console.log(`Unfortunately, there is no such a letter!`);
+        depressionLevel += 1;
+      }
     } else {
-        gameOver('succes', username);
+      console.log("You have entere not a letter.");
     }
+
+    // await delay(1000);
+  }
+
+  if (depressionLevel === 5) {
+    gameOver("fail", username, goalWord);
+  } else {
+    gameOver("succes", username);
+  }
 };
 
 export default hangmanRules;
